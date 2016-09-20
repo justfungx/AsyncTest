7c5114e9d5113b116f88dd606b66a8c8d487f664
justfungx <justfungx@hotmail.com>
2016-09-20 10:29:02 +0800

    V3

--- app/src/main/java/tw/org/iii/asynctest/MainActivity.java
+++ app/src/main/java/tw/org/iii/asynctest/MainActivity.java
@@ -5,74 +5,77 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
-private TextView masg;
     private MyTask mt1;
-
+    private TextView masg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       masg = (TextView)findViewById(R.id.masg);
-
+        masg = (TextView)findViewById(R.id.masg);
     }
     public void test1(View v){
         mt1 = new MyTask();
-        mt1.execute("SHOA","TONY","DK","GX","ERIC");
+        mt1.execute("Brad","Kevin","Tony","Peter","Eric");
     }
     public void test2(View v){
-    if(mt1 != null && mt1.isCancelled()){
-        mt1.cancel(true);
-    }
-    }
-
-    private class MyTask extends AsyncTask<String ,Object,Void>{
-
-
-        @Override
-        protected Void doInBackground(String... params) {
-
-            Log.d("DK","doInBackground");
-
-            for(String name: params){
-                Log.d("DK","HI,"+ name);
-                try{
-                    Thread.sleep(500);
-                }catch (InterruptedException ee){
-
-                }
-            }
-
-            return null;
+        if (mt1 != null && !mt1.isCancelled()){
+            mt1.cancel(true);
         }
+    }
+
+    private class MyTask extends AsyncTask<String,Object,String>{
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            Log.d("DK","doInBackground");
+            Log.d("DK", "onPreExecute");
         }
-
         @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            Log.d("DK","doInBackground");
+        protected String doInBackground(String... params) {
+            Log.d("DK", "doInBackground");
+
+            int i = 0; boolean isCancel = false;
+            for (String name : params){
+                if (isCancelled()){
+                    isCancel = true;
+                    break;
+                }
+
+                Log.d("DK", "HoLa, " + name);
+                i++;
+                publishProgress(i, name);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+
+            return isCancel?"Cancel!":"Game Over";
+        }
+        @Override
+        protected void onPostExecute(String end) {
+            super.onPostExecute(end);
+            Log.d("DK", "onPostExecute:" + end);
         }
 
         @Override
         protected void onProgressUpdate(Object... values) {
             super.onProgressUpdate(values);
-            Log.d("DK","doInBackground");
+            Log.d("DK", "onProgressUpdate");
             masg.setText((Integer)values[0] + ":" + (String)values[1]);
         }
 
         @Override
-        protected void onCancelled(Void aVoid) {
-            super.onCancelled(aVoid);
-            Log.d("DK","doInBackground");
+        protected void onCancelled(String end) {
+            super.onCancelled(end);
+            Log.d("DK", "onCancelled:" +end);
         }
     }
+
 }
